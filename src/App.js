3891047a53a9a4964cc0a0d6cdce36a47de67231
tomeggers <tomeggers741@gmail.com>
2025-08-06 21:46:57 +1200

    Changed the nav bar on mobile to be at the bottom of the screen to be more accessible

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import BookNow from './pages/Book_now';
 import Price from './pages/Price_list';
+import LogoHeader from './components/LogoHeader';
+import MobileNav from './components/MobileNav';
+import './assets/styles/app.css'; // Assuming you have a global styles file
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/price" element={<Price />} />
-        <Route path="/book" element={<BookNow />} />
-      </Routes>
+      <LogoHeader />
+      <MobileNav />
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/price" element={<Price />} />
+          <Route path="/book" element={<BookNow />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
